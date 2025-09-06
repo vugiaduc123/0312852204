52ac12a254e5099495094cf8fa53b8be5fa68077
GiaDuc <vugiaduc123@gmail.com>
2025-09-06 20:13:31 +0700

    up isAteApple

--- main.cpp
+++ main.cpp
@@ -413,3 +413,12 @@ void drawHeadnTail()
     gotoxy(prevTail.x, prevTail.y);
     std::cout << ' ' << std::flush;
 }
+
+bool isBiteItself()
+{
+    Point head = snake[0];
+    for (size_t i = 1; i < snake.size(); i++)
+        if (head.x == snake[i].x && head.y == snake[i].y)
+            return true;
+    return false;
+}
